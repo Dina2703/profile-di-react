e8c91efc576f7ab3888423544cdc167dd4fc33aa
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-02-22 13:29:23 -0500

    added NotFound Page and redirect routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Work from "./pages/Work";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/shared/Layout";
 import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 
@@ -64,11 +65,13 @@ function App() {
           <CssBaseline />
           <Layout>
             <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-              <Route path="/work" element={<Work />}></Route>
-              <Route path="/blogs" element={<Blogs />}></Route>
-              <Route path="/contact" element={<Contact />}></Route>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/work" element={<Work />} />
+              <Route path="/blogs" element={<Blogs />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
             </Routes>
           </Layout>
         </ThemeProvider>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Container, Typography, Grid, Box } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import SentimentVeryDissatisfiedIcon from "@material-ui/icons/SentimentVeryDissatisfied";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    height: "90vh",
+  },
+  container: {
+    height: "100%",
+  },
+  homeIcon: {
+    fontSize: 40,
+    marginBottom: -10,
+    marginLeft: 10,
+  },
+  face: {
+    fontSize: 140,
+  },
+}));
+
+function NotFound() {
+  const classes = useStyles();
+  return (
+    <>
+      <Paper className={classes.section}>
+        <Container maxWidth="md" className={classes.container}>
+          <Grid
+            container
+            justifyContent="center"
+            alignContent="center"
+            className={classes.container}
+          >
+            <Box>
+              <SentimentVeryDissatisfiedIcon className={classes.face} />
+              <Typography variant="h3">404</Typography>
+              <Typography>Oooops, page not found</Typography>
+
+              <Link to="/">
+                <Typography variant="body1" display="inline">
+                  Back to Home
+                </Typography>
+                <HomeIcon className={classes.homeIcon} />
+              </Link>
+            </Box>
+          </Grid>
+        </Container>
+      </Paper>
+    </>
+  );
+}
+
+export default NotFound;
